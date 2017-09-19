f5ba1de366f293610cb0333d0f65ecbe7e06c0d8
Bobey <bobey.valentin@gmail.com>
2017-09-19 11:51:54 +0200

    intégration de tranche age dans Jouet.js et server.js

--- Jouet.js
+++ Jouet.js
@@ -1,11 +1,16 @@
 class Jouet {
 
-    constructor(unLibelle){
+    constructor(unLibelle, uneTrancheAge){
         this._libelle = unLibelle;
+        this._trancheAge = uneTrancheAge;
     }
 
     get libelle(){
         return this._libelle;
+    }
+
+    get trancheAge(){
+        return this._trancheAge.toString();
     }
 
 }
