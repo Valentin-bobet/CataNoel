6c67e70cfd02a2b00b3b2ff3cd89856d25ac8aa4
Bobey <bobey.valentin@gmail.com>
2017-09-26 09:38:04 +0200

    test unitaire

--- Categorie.js
+++ Categorie.js
@@ -19,5 +19,9 @@ class Categorie {
         });
     }
 
+    get_nb_jouets(){
+        return this._lesJouets.length;
+    }
+
 }
 module.exports = Categorie;
---
+++ test/test_categorie.js
@@ -0,0 +1,29 @@
+import * as describe from "describe";
+
+const Categorie = require('../Categorie');
+const TrancheAge = require('../trancheAge');
+const Jouet = require('../Jouet');
+
+const assert = require('assert');
+
+describe("Categorie",() =>{
+
+    describe("ajouterJouet",() => {
+        let categorie1;
+        let unJouet;
+        let tranche25;
+
+        before(function () {
+            categorie1 = new Categorie("Plein Air");
+            tranche25 = new TrancheAge("tranche25",2,5);
+            unJouet = new Jouet("Draisienne junior bleue",tranche25,categorie1);
+        });
+
+        it("Ajoute d'un jouet",() =>{
+
+            assert.equal(categorie1.get_nb_jouets(),1,'Ajout non réalisé');
+        });
+
+
+    });
+});
