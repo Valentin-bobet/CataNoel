b9a6a897d11c15018ea271e7338e22ed7ae7a145
Bobey <bobey.valentin@gmail.com>
2017-09-26 12:00:19 +0200

    facilitation de la liste déroulante

--- trancheAge.js
+++ trancheAge.js
@@ -1,6 +1,7 @@
 class TrancheAge {
 
-    construcor(libelle, ageMin,ageMax){
+    construcor(id,libelle, ageMin,ageMax){
+        this._id = id;
         this._libelle = libelle;
         this._ageMax = ageMax;
         this._ageMin = ageMin;
