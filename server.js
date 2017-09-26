8c023846c44f3afd9a35ae16291c1232e044516f
Bobey <bobey.valentin@gmail.com>
2017-09-26 09:43:26 +0200

    passage en web express

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
-const Jouet = require('./Jouet.js');
-const TrancheAge = require('./trancheAge.js');
-const Categorie = require('./Categorie');
+const express = require('express');
+const app = express();
 
-console.log("Bienvenue dans le catalogue de jouet");
+app.get('/', function (req,res) {
+    res.send('Bienvenue dans le catalogue de jouet');
+});
 
-let tranche25 = new TrancheAge("tranche25",2,5);
-//console.log(tranche25.toString());
-
-let unJouet = new Jouet("Draisienne junior bleue", tranche25);
-//console.log(unJouet.trancheAge);
-
-let categorie1 = new Categorie("Plein Air");
-//console.log(categorie1.libelle);
+app.listen(8081);
