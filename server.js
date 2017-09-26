f0d7be764fee14a726164055f28ce6f87efd4d27
Bobey <bobey.valentin@gmail.com>
2017-09-26 08:59:38 +0200

    oubli dans le commit précédent

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const Categorie = require('./Categorie');
 console.log("Bienvenue dans le catalogue de jouet");
 
 let tranche25 = new TrancheAge("tranche25",2,5);
-console.log(tranche25.toString());
+//console.log(tranche25.toString());
 
 let unJouet = new Jouet("Draisienne junior bleue", tranche25);
-console.log(unJouet.trancheAge);
+//console.log(unJouet.trancheAge);
 
 let categorie1 = new Categorie("Plein Air");
-console.log(categorie1.libelle);
+//console.log(categorie1.libelle);
